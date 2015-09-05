e4f2339caa1cc3f2af8547470eb97d9b32f5a7c5
Joseph Fusco <hello@josephfus.co>
2015-09-05 11:32:22 -0400

    update color picker

--- js/app.js
+++ js/app.js
@@ -1,22 +1,16 @@
 (function($) {
 
-	$("#color").spectrum({
-	    preferredFormat: "hex",
-	    showInput: true
-	});
-	function updateFill(element, fill) {
-	    var hexColor = "transparent";
-	    if (fill) hexColor = fill.toHexString();
-	    $(element).css("fill", hexColor);
-	}
-
-	$("#color").spectrum({
-	    fill: "#333",
-	    preferredFormat: "hex",
-	    showInput: true,
-	    hide: function (fill) {
-	        updateFill(".item-wrap svg", fill);
+	  $('#color').ColorPicker({
+	    onSubmit: function(hsb, hex, rgb, el){
+	      $(el).ColorPickerHide();
+	      $(el).val('#'+hex);
+	    },
+	    onBeforeShow: function (){
+	      $(this).ColorPickerSetColor(this.value);
+	    },
+	    onChange: function(hsb, hex, rgb, el){
+	      $('.item-wrap').css('fill', '#'+hex);
 	    }
-	});
+	  });
 
 })(jQuery);
